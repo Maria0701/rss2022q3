1ed90fc44f0492e94bd3a13a16134c76f52e56f3
Masha0701 <reflection2008@gmail.com>
2022-12-16 19:57:40 +0300

    first commit

---
+++ src/pages/CatalogPage.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export function CatalogPage() {
+    return (
+    <div className="container">
+      страница каталога
+    </div>
+    );
+};
---
+++ src/pages/ProductPage.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export function ProductPage() {
+    return (
+    <div className="container">
+      страница товара
+    </div>
+    );
+};

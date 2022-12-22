3b3b7d5e73a5e787f355b5723f228ef530d0a8a2
Valery <valer4ik-larik@mail.ru>
2022-12-22 22:56:25 +0300

    feat: add some product

--- src/jsons/links.ts
+++ src/jsons/links.ts
@@ -43,7 +43,279 @@ export const PRODUCTS: IProductCard[] = [
       "category":"men's clothing",
       "image":"https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
       "rating":{"rate":4.1,"count":259}
-  }  
+  },  
+  {
+    "id":3,
+    "title":"iPhone 9",
+    "price": 549,
+    "description":"An apple mobile which is nothing like apple",
+    "category":"smartphones",
+    "image":"https://i.dummyjson.com/data/products/1/1.jpg",
+    "rating":{"rate":4.69,"count":120}
+  },
+  {
+    "id":4,
+    "title":"iPhone X",
+    "price":899,
+    "description":"SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+    "category":"smartphones",
+    "image":"https://i.dummyjson.com/data/products/2/1.jpg",
+    "rating":{"rate":4.44,"count":259}
+  }, 
+  {
+    "id":5,
+    "title":"Samsung Universe 9",
+    "price": 1249,
+    "description":"Samsung's new variant which goes beyond Galaxy to the Universe",
+    "category":"smartphones",
+    "image":"https://i.dummyjson.com/data/products/3/1.jpg",
+    "rating":{"rate":4.09,"count":120}
+  },
+  {
+    "id":6,
+    "title":"OPPOF19",
+    "price": 280,
+    "description":"OPPO F19 is officially announced on April 2021.",
+    "category":"smartphones",
+    "image":"https://i.dummyjson.com/data/products/4/1.jpg",
+    "rating":{"rate":4.3,"count":120}
+  },
+  {
+    "id":7,
+    "title":"Huawei P30",
+    "price": 499,
+    "description":"Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+    "category":"smartphones",
+    "image":"https://i.dummyjson.com/data/products/5/1.jpg",
+    "rating":{"rate":4.09,"count":120}
+  },
+  {
+    "id":8,
+    "title":"MacBook Pro",
+    "price": 1749,
+    "description":"MacBook Pro 2021 with mini-LED display may launch between September, November",
+    "category":"laptops",
+    "image":"https://i.dummyjson.com/data/products/6/1.png",
+    "rating":{"rate":4.57,"count":120}
+  },
+  {
+    "id":9,
+    "title":"Samsung Galaxy Book",
+    "price": 1499,
+    "description":"Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
+    "category":"laptops",
+    "image":"https://i.dummyjson.com/data/products/7/1.jpg",
+    "rating":{"rate":4.25,"count":120}
+  },
+  {
+    "id":10,
+    "title":"Microsoft Surface Laptop 4",
+    "price": 1499,
+    "description":"Style and speed. Stand out on HD video calls backed by Studio Mics. Capture ideas on the vibrant touchscreen.",
+    "category":"laptops",
+    "image":"https://i.dummyjson.com/data/products/8/1.jpg",
+    "rating":{"rate":4.43,"count":120}
+  },
+  {
+    "id":11,
+    "title":"Infinix INBOOK",
+    "price": 1099,
+    "description":"Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey – 1 Year Warranty",
+    "category":"laptops",
+    "image":"https://i.dummyjson.com/data/products/9/1.jpg",
+    "rating":{"rate":4.54,"count":120}
+  },
+  {
+    "id":12,
+    "title":"HP Pavilion 15-DK1056WM",
+    "price": 1099,
+    "description":"HP Pavilion 15-DK1056WM Gaming Laptop 10th Gen Core i5, 8GB, 256GB SSD, GTX 1650 4GB, Windows 10",
+    "category":"laptops",
+    "image":"https://i.dummyjson.com/data/products/10/1.jpg",
+    "rating":{"rate":4.43,"count":120}
+  },
+  {
+    "id":13,
+    "title":"perfume Oil",
+    "price": 13,
+    "description":"Mega Discount, Impression of Acqua Di Gio by GiorgioArmani concentrated attar perfume Oil",
+    "category":"fragrances",
+    "image":"https://i.dummyjson.com/data/products/11/1.jpg",
+    "rating":{"rate":4.26,"count":120}
+  },
+  {
+    "id":14,
+    "title":"Brown Perfume",
+    "price": 40,
+    "description":"Royal_Mirage Sport Brown Perfume for Men & Women - 120ml",
+    "category":"fragrances",
+    "image":"https://i.dummyjson.com/data/products/12/1.jpg",
+    "rating":{"rate":4,"count":120}
+  },
+  {
+    "id":15,
+    "title":"Fog Scent Xpressio Perfume",
+    "price": 13,
+    "description":"Product details of Best Fog Scent Xpressio Perfume 100ml For Men cool long lasting perfumes for Men",
+    "category":"fragrances",
+    "image":"https://i.dummyjson.com/data/products/13/1.jpg",
+    "rating":{"rate":4.59,"count":120}
+  },
+  {
+    "id":16,
+    "title":"Non-Alcoholic Concentrated Perfume Oil",
+    "price": 120,
+    "description":"Product details of Best Fog Scent Xpressio Perfume 100ml For Men cool long lasting perfumes for Men",
+    "category":"fragrances",
+    "image":"https://i.dummyjson.com/data/products/14/1.jpg",
+    "rating":{"rate":4.21,"count":120}
+  },
+  {
+    "id":17,
+    "title":"Eau De Perfume Spray",
+    "price": 30,
+    "description":"Genuine  Al-Rehab spray perfume from UAE/Saudi Arabia/Yemen High Quality",
+    "category":"fragrances",
+    "image":"https://i.dummyjson.com/data/products/15/1.jpg",
+    "rating":{"rate":4.7,"count":120}
+  },
+  {
+    "id":18,
+    "title":"Hyaluronic Acid Serum",
+    "price": 19,
+    "description":"L'OrÃ©al Paris introduces Hyaluron Expert Replumping Serum formulated with 1.5% Hyaluronic Acid",
+    "category":"skincare",
+    "image":"https://i.dummyjson.com/data/products/16/1.png",
+    "rating":{"rate":4.83,"count":120}
+  },
+  {
+    "id":19,
+    "title":"Tree Oil 30ml",
+    "price": 12,
+    "description":"Tea tree oil contains a number of compounds, including terpinen-4-ol, that have been shown to kill certain bacteria,",
+    "category":"skincare",
+    "image":"https://i.dummyjson.com/data/products/17/1.jpg",
+    "rating":{"rate":4.52,"count":120}
+  },
+  {
+    "id":20,
+    "title":"Oil Free Moisturizer 100ml",
+    "price": 40,
+    "description":"Dermive Oil Free Moisturizer with SPF 20 is specifically formulated with ceramides, hyaluronic acid & sunscreen.",
+    "category":"Dermive",
+    "image":"https://i.dummyjson.com/data/products/18/1.jpg",
+    "rating":{"rate":4.56,"count":120}
+  },
+  {
+    "id":21,
+    "title":"Skin Beauty Serum.",
+    "price": 26,
+    "description":"Product name: rorec collagen hyaluronic acid white face serum riceNet weight: 15 m",
+    "category":"skincare",
+    "image":"https://i.dummyjson.com/data/products/19/1.jpg",
+    "rating":{"rate":4.42,"count":120}
+  },
+  {
+    "id":22,
+    "title":"Freckle Treatment Cream- 15gm",
+    "price": 70,
+    "description":"Fair & Clear is Pakistan's only pure Freckle cream which helpsfade Freckles, Darkspots and pigments. Mercury level is 0%, so there are no side effects.",
+    "category":"skincare",
+    "image":"https://i.dummyjson.com/data/products/20/1.jpg",
+    "rating":{"rate":4.06,"count":120}
+  },
+  {
+    "id":23,
+    "title":"- Daal Masoor 500 grams",
+    "price": 20,
+    "description":"Fine quality Branded Product Keep in a cool and dry place",
+    "category":"groceries",
+    "image":"https://i.dummyjson.com/data/products/21/1.png",
+    "rating":{"rate":4.44,"count":120}
+  },
+  {
+    "id":24,
+    "title":"Elbow Macaroni - 400 gm",
+    "price": 14,
+    "description":"Product details of Bake Parlor Big Elbow Macaroni - 400 gm",
+    "category":"groceries",
+    "image":"https://i.dummyjson.com/data/products/22/1.jpg",
+    "rating":{"rate":4.57,"count":120}
+  },
+  {
+    "id":25,
+    "title":"Orange Essence Food Flavou",
+    "price": 14,
+    "description":"Specifications of Orange Essence Food Flavour For Cakes and Baking Food Item",
+    "category":"groceries",
+    "image":"https://i.dummyjson.com/data/products/23/1.jpg",
+    "rating":{"rate":4.85,"count":120}
+  },
+  {
+    "id":26,
+    "title":"cereals muesli fruit nuts",
+    "price": 46,
+    "description":"original fauji cereal muesli 250gm box pack original fauji cereals muesli fruit nuts flakes breakfast cereal break fast faujicereals cerels cerel foji fouji",
+    "category":"groceries",
+    "image":"https://i.dummyjson.com/data/products/24/1.jpg",
+    "rating":{"rate":4.65,"count":120}
+  },
+  {
+    "id":27,
+    "title":"Gulab Powder 50 Gram",
+    "price": 46,
+    "description":"Dry Rose Flower Powder Gulab Powder 50 Gram • Treats Wounds",
+    "category":"groceries",
+    "image":"https://i.dummyjson.com/data/products/25/1.png",
+    "rating":{"rate":4.87,"count":120}
+  },
+  {
+    "id":28,
+    "title":"Plant Hanger For Home",
+    "price": 41,
+    "description":"Boho Decor Plant Hanger For Home Wall Decoration Macrame Wall Hanging Shelf",
+    "category":"home-decoration",
+    "image":"https://i.dummyjson.com/data/products/26/thumbnail.jpg",
+    "rating":{"rate":4.08,"count":120}
+  },
+
+  {
+    "id": 29,
+    "title": "Flying Wooden Bird",
+    "description": "Package Include 6 Birds with Adhesive Tape Shape: 3D Shaped Wooden Birds Material: Wooden MDF, Laminated 3.5mm",
+    "price": 51,
+    "category":"home-decoration",
+    "image":"https://i.dummyjson.com/data/products/27/thumbnail.webp",
+    "rating":{"rate":4.41,"count":120}
+  },
+  
+    {
+    "id": 30,
+    "title": "3D Embellishment Art Lamp",
+    "description": "3D led lamp sticker Wall sticker 3d wall art light on/off button  cell operated (included)",
+    "price": 20,
+    "category": "home-decoration",
+    "image": "https://i.dummyjson.com/data/products/28/thumbnail.jpg",
+    "rating":{"rate":4.82,"count":120}
+    },
+    {
+    "id": 31,
+    "title": "Handcraft Chinese style",
+    "description": "Handcraft Chinese style art luxury palace hotel villa mansion home decor ceramic vase with brass fruit plate",
+    "price": 60,
+    "category": "home-decoration",
+    "image": "https://i.dummyjson.com/data/products/29/thumbnail.webp",
+    "rating":{"rate":4.44,"count":120}
+    },
+    {
+    "id": 32,
+    "title": "Key Holder",
+    "description": "Attractive DesignMetallic materialFour key hooksReliable & DurablePremium Quality",
+    "price": 30,
+    "category": "home-decoration",
+    "image": "https://i.dummyjson.com/data/products/30/thumbnail.jpg",
+    "rating":{"rate":4.92,"count":120}
+    }
 ];
 
 

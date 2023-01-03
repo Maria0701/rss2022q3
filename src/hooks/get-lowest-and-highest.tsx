a4eeda8c32c85e31477f9165bce27e5db34cdbcd
Masha0701 <reflection2008@gmail.com>
2023-01-03 14:43:20 +0300

    feat: console logs removed

--- src/hooks/get-lowest-and-highest.tsx
+++ src/hooks/get-lowest-and-highest.tsx
@@ -5,14 +5,11 @@ interface IMinMax {
 };
 
 export function getHighestAndLowest(arr:IProductCard[]):IMinMax {
-    console.log(arr);
     if (arr.length === 0) {return {
         min: 0,
         max: 0,
     }}
     const arrPrices = arr.map(item => item.price || 0);
-    console.log({min: Math.min(...arrPrices),
-    max: Math.max(...arrPrices)},'function')
     return {
         min: Math.min(...arrPrices),
         max: Math.max(...arrPrices),

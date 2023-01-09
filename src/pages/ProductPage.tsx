78c3925a07231a732b6409f1b79a5fa31b82c72d
Maria0701 <reflection2008@gmail.com>
2023-01-09 20:17:25 +0300

    feat: correction

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -5,8 +5,6 @@ import { Rating } from "../components/stars/stars";
 import { useAppDispatch, useAppSelector } from "../hooks/reducer";
 import { fetchSingleProduct } from "../store/productSlice";
 import './product.css';
-import left from "../image/left.png"
-import right from "../image/right.png"
 import { ImageSlider } from "../components/imageSlider/imageSlider";
 
 
@@ -37,7 +35,7 @@ export function ProductPage() {
         <h4>{product.category}</h4>
         <Rating rating={product.rating}/>
         <h3>{product.price}$</h3>
-        <AddProductToCart id={product.id} text='add to cart' title={product.title} price={product.price}/>
+        <AddProductToCart id={product.id} text='add to cart' title={product.title} price={product.price} stock={product.stock}/>
         <p>{product.description}</p>
       </div>
     </div>
